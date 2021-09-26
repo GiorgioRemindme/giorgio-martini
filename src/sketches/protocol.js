5d9e865a3667273ba3b3158a4db88fadab6c8656
jorge <jorge@remind.me>
2021-09-26 23:05:32 +0200

    addes rotating animation

--- src/sketches/protocol.js
+++ src/sketches/protocol.js
@@ -13,113 +13,114 @@ function evenRandomNumber(amt) { // add to utils
 }
 
 function Sketch(p5) {
-  function squares(opts) {
-    let squaresRadius 
-    let angle
-    let amt
-   
-
-    // if size is small use fewe squares 2
-    function display() {
-      squaresRadius = p5.random(p5.width/8, opts.squareSize)
-      console.log("squaresRadius: ", squaresRadius);
-      amt = Math.ceil(p5.random(opts.amt))
-      if (squaresRadius < 160) amt = 2 // make it into a p5.map
+  // function squares(opts) {
+  //   let squaresRadius 
+  //   let angle
+  //   let amt
+  //   // if size is small use fewe squares 2
+  //   function display() {
+  //     squaresRadius = p5.random(p5.width/8, opts.squareSize)
+  //     console.log("squaresRadius: ", squaresRadius);
+  //     amt = Math.ceil(p5.random(opts.amt))
+  //     if (squaresRadius < 160) amt = 2 // make it into a p5.map
       
-      angle = (360/amt)/4
-      if(opts.strokeWeight) {
-        opts.strokeWeight && p5.noFill() 
-        opts.strokeWeight && p5.strokeWeight(p5.random(2, opts.strokeWeight)) 
-        opts.strokeWeight && p5.stroke(color)  
-      }
+  //     angle = (360/amt)/4
+  //     if(opts.strokeWeight) {
+  //       opts.strokeWeight && p5.noFill() 
+  //       opts.strokeWeight && p5.strokeWeight(p5.random(2, opts.strokeWeight)) 
+  //       opts.strokeWeight && p5.stroke(color)  
+  //     }
       
-      !opts.strokeWeight && p5.fill(color)
-      !opts.strokeWeight && p5.noStroke()
-      p5.angleMode(p5.DEGREES)
+  //     !opts.strokeWeight && p5.fill(color)
+  //     !opts.strokeWeight && p5.noStroke()
+  //     p5.angleMode(p5.DEGREES)
       
 
-      for (let i = 0; i < amt; i++) {
-        p5.push()
-        p5.rotate(angle * i)
-        p5.rectMode(p5.CENTER)
-        p5.rect(0, 0, squaresRadius, squaresRadius)
-        p5.pop()
-      }    
-    }
+  //     for (let i = 0; i < amt; i++) {
+  //       p5.push()
+  //       p5.rotate(angle * i)
+  //       p5.rectMode(p5.CENTER)
+  //       p5.rect(0, 0, squaresRadius, squaresRadius)
+  //       p5.pop()
+  //     }    
+  //   }
   
-    return {display}
-  }
+  //   return {
+  //     display
+  //   }
+  // }
 
-  function crop(opts) {
+  function circles(opts) {
     let r 
     let angle
     let amt
+    let posArray = []
+    let animation = 30
+    let resting = 40
    
-    function display() {
+    function generatePositions() {
+      p5.angleMode(p5.DEGREES)
       r = p5.random(5, opts.maxCircleSize)
-      
       amt = evenRandomNumber(opts.amt)
       angle = 360 / amt
       let cropRadius = p5.random(opts.cropRadius[0], opts.cropRadius[1])
-  
+
+      // SETTINGS 
       if(opts.strokeWeight) {
         const [strokeMin, strokeMax] = opts.strokeWeight
         opts.strokeWeight && p5.noFill() 
         opts.strokeWeight && p5.strokeWeight(p5.random(strokeMin, strokeMax)) 
         opts.strokeWeight && p5.stroke(color)  
       }
-      
       !opts.strokeWeight && p5.fill(color)
       !opts.strokeWeight && p5.noStroke()
-      p5.angleMode(p5.DEGREES)
       
+      posArray = []
       for (let i = 0; i < amt; i++) {
-        p5.push()
-        p5.rotate(angle * i)
-        p5.translate(cropRadius, 0)
-        p5.ellipse(0, 0, r, r)
-        p5.pop()
+        posArray.push([angle * i, cropRadius])
       }    
     }
-  
-    return {display}
-  }
 
-  function renderCrop () {
-    p5.translate(p5.width / 2, p5.height / 2) 
-    // reset to black
-    p5.blendMode(p5.REPLACE) 
-    p5.background(0)
-    // set to difference to make the ngative space stuff
-    p5.blendMode(p5.DIFFERENCE) 
-    c1.display()
-    c2.display()
-    squares.display()
-    let originalCrops = p5.get()
-    // reset to black agin
-    p5.blendMode(p5.REPLACE) 
-    p5.background(0)
-    p5.blendMode(p5.REPLACE) 
-    p5.blendMode(p5.DIFFERENCE) 
-    c1.display()
-    c2.display()
-    squares.display()
-    p5.filter(p5.INVERT)
-    p5.blendMode(p5.DIFFERENCE) 
-    // p5.push()
-    // p5.image(img, -p5.width/2, -p5.height/2, p5.width, p5.height)
-    // p5.pop()
-  }
+    function display() {
+      if (animation > 1) {
+        animation --
 
-  // p5.preload = () => {
-  //   img = p5.loadImage('https://i.imgur.com/4HEz2Th.jpg'); // TODO make imgurs local
-  // }
+        posArray.forEach((pos, i) => {
+          p5.push()
+            p5.rotate(pos[0] / animation)
+            p5.translate(pos[1], 0)
+            p5.ellipse(0, 0, r, r)
+          p5.pop()
+        })        
+      }
+
+      if (animation === 1 ) {
+        resting --
+        posArray.forEach((pos, i) => {
+          p5.push()
+            p5.rotate(pos[0] / animation)
+            p5.translate(pos[1], 0)
+            p5.ellipse(0, 0, r, r)
+          p5.pop()
+        })        
+
+        if (resting < 1) {
+          animation = 30
+          resting = 40
+          generatePositions()
+        }
+      }
+    }    
+
+    return {
+      generatePositions,
+      display
+    }
+  }
 
   p5.setup = () => {
     p5.createCanvas(600, 600)
-
     p5.translate(p5.width / 2, p5.height / 2) 
-    // p5.image(img, -p5.width/2, -p5.height/2, p5.width, p5.height)
     
     c1Opts = {
       amt: 8,
@@ -139,16 +140,25 @@ function Sketch(p5) {
       strokeWeight: 10,
     }
     
-    c1 = crop(c1Opts)
-    c2 = crop(c2Opts)
-    squares = squares(squaresOpts)
-
+    c1 = circles(c1Opts)
+    c2 = circles(c2Opts)
+    // squares = squares(squaresOpts)
     p5.translate(-p5.width / 2, -p5.height / 2) 
+    c1.generatePositions()
     // setInterval(renderCrop, 2000);
   }
 
   p5.draw = () => {
-    renderCrop()
+    p5.translate(p5.width / 2, p5.height / 2) 
+    // reset to black
+    p5.blendMode(p5.REPLACE) 
+    p5.background(0)
+    // set to difference to make the ngative space stuff
+    p5.blendMode(p5.DIFFERENCE) 
+    c1.display()
+    // c2.display()
+    // squares.display()
+    p5.filter(p5.INVERT)
   }
 }
 
